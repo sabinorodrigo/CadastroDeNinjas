cf7e421c02bbff1c04f144158c17d3856a17a798
Rodrigo Sabino Gomes <sr.rodrigosabino@gmail.com>
2025-05-10 12:17:01 -0300

    Cria uma entidade no banco de dados apartir do ninjamodel

--- src/main/java/com/rsg/CadastroDeNinjas/controller/Controller.java
+++ src/main/java/com/rsg/CadastroDeNinjas/controller/Controller.java
@@ -1,7 +1,6 @@
 package com.rsg.CadastroDeNinjas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ src/main/java/com/rsg/CadastroDeNinjas/model/NinjaModel.java
@@ -0,0 +1,50 @@
+package com.rsg.CadastroDeNinjas.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String nome;
+    @Column
+    private String email;
+    @Column
+    private int idade;
+
+    public NinjaModel() {}
+
+    public NinjaModel(int idade, String email, String nome) {
+        this.idade = idade;
+        this.email = email;
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
